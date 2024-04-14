53db5a67fb6f639b18b0e40e66f994bbacc32d1e
akosuaeffa <130052173+akosuaeffa@users.noreply.github.com>
2024-04-14 23:35:40 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+# api key
+
+api_key = '8bf4fd7d0a90b599c2dd9e7589c0fb94'
---
+++ weather.py
@@ -0,0 +1,37 @@
+import requests
+from flask import Flask, render_template, request, url_for 
+import config
+from urllib.parse import quote
+
+app = Flask(__name__, static_url_path="/static") #initialising the app
+
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+
+@app.route("/weather", methods=['POST'])
+def get_weather():
+    city = request.form['city']
+    city_name = quote(city)
+
+    api_key = config.api_key
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
+
+    response = requests.get(url)
+    weather_data = response.json()
+    # print (weather_data)
+
+    if weather_data["cod"] == 200:
+        data = {
+           "city": weather_data['name'],
+            "temp": weather_data['main']['temp'],
+            "description": weather_data['weather'][0]['description'],
+            "icon": weather_data['weather'][0]['icon']
+        }
+        return render_template('weather.html', data=data)
+    
+    else:
+        return render_template("error.html",  err_ymessage="Page could not be found")
+
+if __name__ == "__main__":
+    app.run(debug=True)
